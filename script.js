edd8e76c6816d4e955add6950edfd4390304fca0
Harrison Leandro <harrisonleandro2022@gmail.com>
2024-07-27 18:42:01 -0300

    brincando com o projeto

---
+++ script.js
@@ -0,0 +1,16 @@
+//Login Page =========================================
+
+var ae = window.document.getElementById('areaemail')
+ae.addEventListener('mouseenter', entrar)
+
+function entrar() {
+    
+}
+
+
+
+//Index Page =========================================
+
+function 
+
+var horario = document.getElementById('horario')
